1e39d9f012b439887f3c52cb17e40d0787521828
Luna <luna.espinosa@usp.br>
2021-07-26 17:19:36 -0300

    XMatch modification

--- get_splus_gals.py
+++ get_splus_gals.py
@@ -1,18 +1,17 @@
+from astroquery.xmatch import XMatch
+from astropy import units as u
 import pandas as pd
-import numpy as np
 import splusdata
-import os
-import time
 
-conn = splusdata.connect('user', 'pass')
+conn = splusdata.connect('luna.espinosa', '741852963_')
 
-fields = pd.read_csv('Sgalaxies.csv')
+fields = pd.read_csv('/home/luna/Documentos/usp/ic/fabio/Sgalaxies.csv')
 
 for field in fields['NAME']:
     print('Starting ' + f'{field}')
-    aperture = 'aper_6'
+    aperture = 'petro'
     try:
-        query_f = f"""SELECT det.ID, det.ra, det.dec, u.u_{aperture}, j0378.j0378_{aperture}, j0395.j0395_{aperture}, j0410.j0410_{aperture}, j0430.j0430_{aperture}, g.g_{aperture}, j0515.j0>
+        query_f = f"""SELECT det.ID, det.ra, det.dec, u.u_{aperture}, u.e_u_{aperture}, j0378.j0378_{aperture}, j0378.e_j0378_{aperture}, j0395.j0395_{aperture}, j0395.e_j0395_{aperture}, j0410.j0410_{aperture}, j0410.e_j0410_{aperture}, j0430.j0430_{aperture}, j0430.e_j0430_{aperture}, g.g_{aperture}, g.e_g_{aperture}, j0515.j0515_{aperture}, j0515.e_j0515_{aperture}, pz.PDF_Means
                       FROM idr3.detection_image as det                                                                                                                                         
                       JOIN idr3.u_band as u ON (u.ID = det.ID)                                                                                                                                 
                       JOIN idr3.j0378_band as j0378 ON (j0378.ID = det.ID)                                                                                                                     
@@ -24,28 +23,23 @@ for field in fields['NAME']:
                       JOIN idr3.r_band as r ON (r.ID = det.ID)                                                                                                                                 
                       JOIN idr3.j0660_band as j0660 ON (j0660.ID = det.ID)                                                                                                                     
                       JOIN idr3.i_band as i ON (i.ID = det.ID)                                                                                                                                 
-                      JOIN idr3.j0861_band as j0861 ON (j0861.ID = det.ID)                                                                                                                     
-                      JOIN idr3.z_band as z ON (z.ID = det.ID)                                                                                                                                 
-                      JOIN idr3_vacs.photoz_pdfs as pz ON (pz.ID = det.ID)                                                                                                                     
-                      WHERE det.field = '{field}'"""
+                      JOIN idr3.j0861_band as j0861 ON (j0861.ID = det.ID)   
+                      JOIN idr3.z_band as z ON (z.ID = det.ID)
+                      JOIN idr3_vacs.photoz as pz ON (pz.ID = det.ID)     
+                      JOIN idr3_vacs.star_galaxy_quasar as sgq ON (sgq.ID = det.ID)                                                                                                                                                                                                                     
+                      WHERE det.field = '{field}' AND sgq.PROB_GAL > 0.9"""
 
         table = conn.query(query_f)
-        print('Got the table!')
-        table.write(f'{field}.fits') ## para salvar tabela
+        print('Got the table!') #astropy.Table
+        table.write(f'{field}_query.fits', overwrite=True) # para salvar tabela
 
-        # ## Fazendo o cross-match com o 2MASS, GALEX e o unWISE.
-        # os.system(f"""java -jar stilts.jar cdsskymatch in={value.field}.fits cdstable=II/246/out ra=RA dec=DEC radius=1 find=each blocksize=500000 ocmd='delcols "RAJ2000 DEJ2000 errHalfMaj>
+        galex = XMatch.query(cat1=table, cat2='vizier:II/335/galex_ais', max_distance=2*u.arcsec, colRA1='RA', colDec1='DEC')
+        wise = XMatch.query(cat1=galex, cat2='vizier:II/328/allwise', max_distance=2*u.arcsec, colRA1='RA', colDec1='DEC')
 
-        # os.system(f"""java -jar stilts.jar cdsskymatch in={value.field}_vac.fits cdstable=II/335/galex_ais ra=RA dec=DEC radius=2 find=each blocksize=500000 ocmd='delcols "RAJ2000 DEJ2000 >
-
-        # os.system(f"""java -jar stilts.jar cdsskymatch in={value.field}_vac.fits cdstable=II/363/unwise ra=RA dec=DEC radius=1 find=each blocksize=500000 ocmd='delcols "RAdeg DEdeg XposW1 >
-
-        # os.system(f"""rm {value.field}.fits""")
-
-        # os.system(f"""rm {value.field}_vac.fits""")
-
+        wise.write(f'{field}_match.csv', overwrite=True)
     except:
         print(f"Error on {field}")
         file = open('error.txt', 'a')
         file.write(f'Error on {field}\n')
         file.close()
+
