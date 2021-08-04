44a9bf39a78f69d9db0264a6bc12eac305535256
Luna <luna.espinosa@usp.br>
2021-08-03 22:09:00 -0300

    XMatch 2.0 (GALEX, WISE, 2MASS)

--- get_splus_gals.py
+++ get_splus_gals.py
@@ -4,14 +4,22 @@ import pandas as pd
 import splusdata
 
 conn = splusdata.connect('luna.espinosa', '741852963_')
-
+dir = '/home/luna/Documentos/usp/ic/fabio/'
 fields = pd.read_csv('/home/luna/Documentos/usp/ic/fabio/Sgalaxies.csv')
 
 for field in fields['NAME']:
     print('Starting ' + f'{field}')
     aperture = 'petro'
     try:
-        query_f = f"""SELECT det.ID, det.ra, det.dec, u.u_{aperture}, u.e_u_{aperture}, j0378.j0378_{aperture}, j0378.e_j0378_{aperture}, j0395.j0395_{aperture}, j0395.e_j0395_{aperture}, j0410.j0410_{aperture}, j0410.e_j0410_{aperture}, j0430.j0430_{aperture}, j0430.e_j0430_{aperture}, g.g_{aperture}, g.e_g_{aperture}, j0515.j0515_{aperture}, j0515.e_j0515_{aperture}, pz.PDF_Means
+        query_f = f"""SELECT det.ID, det.ra, det.dec, u.u_{aperture}, u.e_u_{aperture}, u.PhotoFlag_u, 
+        j0378.j0378_{aperture}, j0378.e_j0378_{aperture}, j0378.PhotoFlag_j0378, j0395.j0395_{aperture}, 
+        j0395.e_j0395_{aperture}, j0395.PhotoFlag_j0395, j0410.j0410_{aperture}, j0410.e_j0410_{aperture}, 
+        j0410.PhotoFlag_j0410, j0430.j0430_{aperture}, j0430.e_j0430_{aperture}, j0430.PhotoFlag_j0430, 
+        g.g_{aperture}, g.e_g_{aperture}, g.PhotoFlag_g, j0515.j0515_{aperture}, j0515.e_j0515_{aperture}, 
+        j0515.PhotoFlag_j0515, r.r_{aperture}, r.e_r_{aperture}, r.PhotoFlag_r, j0660.j0660_{aperture}, 
+        j0660.e_j0660_{aperture}, j0660.PhotoFlag_j0660, i.i_{aperture}, i.e_i_{aperture}, i.PhotoFlag_i, 
+        j0861.j0861_{aperture}, j0861.e_j0861_{aperture}, j0861.PhotoFlag_j0861, z.z_{aperture}, z.e_z_{aperture}, 
+        z.PhotoFlag_z, pz.zml, pz.zml_err
                       FROM idr3.detection_image as det                                                                                                                                         
                       JOIN idr3.u_band as u ON (u.ID = det.ID)                                                                                                                                 
                       JOIN idr3.j0378_band as j0378 ON (j0378.ID = det.ID)                                                                                                                     
@@ -31,12 +39,13 @@ for field in fields['NAME']:
 
         table = conn.query(query_f)
         print('Got the table!') #astropy.Table
-        table.write(f'{field}_query.fits', overwrite=True) # para salvar tabela
+        table.write(f'{dir}{field}_query.csv', overwrite=True)
 
         galex = XMatch.query(cat1=table, cat2='vizier:II/335/galex_ais', max_distance=2*u.arcsec, colRA1='RA', colDec1='DEC')
         wise = XMatch.query(cat1=galex, cat2='vizier:II/328/allwise', max_distance=2*u.arcsec, colRA1='RA', colDec1='DEC')
 
-        wise.write(f'{field}_match.csv', overwrite=True)
+
+        wise.write(f'{dir}{field}_match.csv', overwrite=True)
     except:
         print(f"Error on {field}")
         file = open('error.txt', 'a')
